abbea6724119c021b68b58bc28007d6bb5d74d52
Silvio <seoclientes@npbrasil.com>
2023-11-01 17:04:59 -0300

    atualizar

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    
+    { src: '~/plugins/vueSmoothScroll.js', mode: 'client' },
     { src: '~/plugins/vue-js-modal.js' },
     { src: '~/plugins/vue-mobile-detection.js' },
 
